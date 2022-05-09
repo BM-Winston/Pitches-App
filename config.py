308a093cd2ba6ccac9758c312c3670620cdbcf5b
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-05-09 23:45:22 +0300

    Add classes

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI =
+
+
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig,
+
+}
