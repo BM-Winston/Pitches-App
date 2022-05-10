8f2f28fabfb2bdc8c3870eaf24e948de26e8f9b1
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-05-10 12:35:45 +0300

    Add pitches categories

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,3 +7,13 @@ class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us more about yourself.')
     submit = SubmitField('Submit')
 
+class PitchForm(FlaskForm):
+    category = StringField('Pitch Category',choices = [('Pick-up Lines','Pick-up Lines'),('Interview','Interview'),('Product','Product'),('Promotion','Promotion'),('Music','Music'),('Sports','Sports')]) 
+    context = TextAreaField('Pitch itself')
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    title = StringField('Comment title')
+    comment = TextAreaField('Pitch comment')
+    submit = SubmitField('Submit')
+
