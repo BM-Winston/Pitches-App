99a415670469354cd11f9faa622389d55be79cbf
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-05-10 13:00:03 +0300

    add manager

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,16 @@ db = SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__)
 
-    # ....
+    
 
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
 
-    # ...
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+if __name__ == '__main__':
+    manager.run()
+
