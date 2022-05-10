ebdf944a4d9d02aa9852425641cb6498b798323c
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-05-10 17:38:47 +0300

    Add navbar

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,10 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
 
+    # Setting up configuration
+    app.config.from_object(DevConfig)
+    app.config.from_pyfile("config.py")
+
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
