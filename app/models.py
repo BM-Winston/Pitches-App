45445274716c25a5b3422f579d70953bfd2be3be
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-05-10 11:10:33 +0300

    Add class Users

--- app/models.py
+++ app/models.py
@@ -1,1 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from ..app import db
+
+db = SQLAlchemy
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
